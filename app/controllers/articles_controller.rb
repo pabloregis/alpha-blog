42319309ccbab3f657ecfe5c771962a0323615b5
Pablo Regis <pablo.sregis@gmail.com>
2018-01-31 22:41:01 +0000

    fixed missing keywords on create action on articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   end
   
   #CREATE ------------------------------------------------------------
-  def 
+  def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.user = User.first
